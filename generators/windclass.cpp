9eda1850209cea53123e883b51ae50893115930c
Noah Maze <nbm39@cornell.edu>
2013-12-24 23:09:02 -0800

    added piecewise linear model and dev test nbm_test.glm

--- generators/windclass.cpp
+++ generators/windclass.cpp
@@ -41,12 +41,25 @@ windclass::windclass(MODULE *module)
 			GL_THROW("unable to register object class implemented by %s", __FILE__); 
 
 		if (gl_publish_variable(oclass,
-			PT_enumeration,"Gen_mode",PADDR(Gen_mode),
-				PT_KEYWORD,"MY_ZERO",(enumeration)MY_ZERO,
-				PT_KEYWORD,"MY_CONSTANT",(enumeration)MY_CONSTANT,
-				PT_KEYWORD,"MY_RANDOM",(enumeration)MY_RANDOM,
+			PT_enumeration,"Model_type",PADDR(Model_type),
+				PT_KEYWORD,"LINEAR",(enumeration)LINEAR,
+				PT_KEYWORD,"LOGISTIC",(enumeration)LOGISTIC,
+				PT_KEYWORD,"AGGLINEAR",(enumeration)AGGLINEAR,
+				PT_KEYWORD,"AGGLOGISTIC",(enumeration)AGGLOGISTIC,
 
-			PT_double, "a_new_double", PADDR(a_new_double),
+			PT_double, "power_out", PADDR(power_out),
+			PT_double, "cut_in", PADDR(cut_in),
+			PT_double, "cut_out", PADDR(cut_out),
+			PT_double, "pA", PADDR(pA),
+			PT_double, "wA", PADDR(wA),
+			PT_double, "pB", PADDR(pB),
+			PT_double, "wB", PADDR(wB),
+			PT_double, "pC", PADDR(pC),
+			PT_double, "wC", PADDR(wC),
+			PT_double, "pD", PADDR(pD),
+			PT_double, "wD", PADDR(wD),
+			PT_double, "pE", PADDR(pE),
+			PT_double, "wE", PADDR(wE),
 			NULL)<1) GL_THROW("unable to publish properties in %s",__FILE__);
 		defaults = this;
 
@@ -61,17 +74,75 @@ int windclass::create(void)
 {
 	memcpy(this,defaults,sizeof(*this));
 	/* TODO: set the context-free initial value of properties */
-	a_new_double = 1.0;
-	Gen_mode = 1;
+	power_out = 1.0;
+	Model_type = 1;
+
+	// Params for all models
+	 cut_in = 5.0;
+	 cut_out = 30.0;
+	
+	// =Linear Model Parameters
+	 wA=6.0; 	 pA=20.0;
+	 wB=10.0;	 pB=75.0;
+	 wC=14.0;	 pC=140.0;
+	 wD=15.0;	 pD=135.0;
+	 wE=30.0;	 pE=130.0;	
+
 	return 1; /* return 1 on success, 0 on failure */
 }
+
+/* Checks for climate object and maps the climate variables to the windturb object variables.  
+If no climate object is linked, then default pressure, temperature, and wind speed will be used. */
+int windclass::init_climate()
+{
+	OBJECT *hdr = OBJECTHDR(this);
+
+	// link to climate data
+	static FINDLIST *climates = NULL;
+	int not_found = 0;
+	
+	climates = gl_find_objects(FL_NEW,FT_CLASS,SAME,"climate",FT_END);
+	if (climates==NULL)
+	{
+		not_found = 1;
+		gl_warning("windturb_dg (id:%d)::init_climate(): no climate data found",hdr->id);
+				
+	}
+	else if (climates->hit_count>1)
+	{
+		gl_verbose("windturb_dg: %d climates found, using first one defined", climates->hit_count);
+	}
+	if (climates!=NULL)	
+	{
+		if (climates->hit_count==0)	
+		{
+			//default to mock data
+			gl_warning("windturb_dg (id:%d)::init_climate(): no climate data found, using static data",hdr->id);
+		}
+		else //climate data was found
+		{
+			// force rank of object w.r.t climate
+			OBJECT *obj = gl_find_next(climates,NULL);
+			if (obj->rank<=hdr->rank)
+				gl_set_dependent(obj,hdr);
+			pWS = (double*)GETADDR(obj,gl_get_property(obj,"wind_speed"));
+
+			//Make sure it worked
+			if (pWS==NULL)
+			{
+				GL_THROW("windturb_dg (id:%d): Unable to map wind_speed from climate object",obj->id);
+			}
+		}
+	}
+	return 1;
+}
+
 
 /* Object initialization is called once after all object have been created */
 int windclass::init(OBJECT *parent)
 {
-
+	init_climate();
 	gl_verbose("windclass init: about to exit");
-	//init_climate();
 	return 1; /* return 1 on success, 0 on failure */
 }
 
@@ -88,17 +159,25 @@ TIMESTAMP windclass::presync(TIMESTAMP t0, TIMESTAMP t1)
 /* Sync is called when the clock needs to advance on the bottom-up pass */
 TIMESTAMP windclass::sync(TIMESTAMP t0, TIMESTAMP t1) 
 {
-	switch(Gen_mode) 
+	switch(Model_type) 
 	{
-		case MY_ZERO:	a_new_double += 0;
+		case LINEAR:	power_out = 0.0;
+				if((*pWS) < wE) power_out = pE - ((pE - pD)*(wE-(*pWS)) / (wE - wD));
+				if((*pWS) < wD) power_out = pD - ((pD - pC)*(wD-(*pWS)) / (wD - wC));
+				if((*pWS) < wC) power_out = pC - ((pC - pB)*(wC-(*pWS)) / (wC - wB));
+				if((*pWS) < wB) power_out = pB - ((pB - pA)*(wB-(*pWS)) / (wB - wA));
+				if((*pWS) < wA) power_out = pA - ((pA     )*(wA-(*pWS)) / (wA - cut_in));
+				if((*pWS) < cut_in) power_out = 0.0;
 				break;
-		case MY_CONSTANT: a_new_double += 0.01;
+		case LOGISTIC: power_out = 10.0 * (*pWS);
 				break;
-		case MY_RANDOM: a_new_double += rand();
+		case AGGLINEAR: power_out = (rand() % 100)* (*pWS)/50.0;
+				break;
+		case AGGLOGISTIC: power_out = (rand() % 100)* (*pWS)/50.0;
 				break;
 	}
 
-	return t1+60;
+	return t1+600;
 }
 
 /* Postsync is called when the clock needs to advance on the second top-down pass */
